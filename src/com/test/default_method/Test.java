062c54c6743f2185fa7d7f59a93793bbb8c03592
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-08-08 07:12:14 +0530

    static method introduced

--- src/com/test/default_method/Test.java
+++ src/com/test/default_method/Test.java
@@ -3,8 +3,11 @@ package com.test.default_method;
 public class Test implements A, B{
 
     public static void main(String[] args) {
+
         Test test = new Test();
         test.getName();
+        System.out.println(A.getFirstName()); // This is a drawback of static methods. We can not call them via the object of implemented class.
+        //we need to call it directly By interface name
     }
 
     //this is the solution for diamond problem
